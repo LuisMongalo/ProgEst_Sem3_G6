edeabb6bedec9030bf842436cee4f1321de765ae
Luis <luis.mongalo@uamv.edu.ni>
2026-08-31 10:31:22 -0600

    se actualizo el archivo

--- Excepciones/Calculadora_edad.py
+++ Excepciones/Calculadora_edad.py
@@ -1,5 +1,10 @@
-try:
-    edad = int(input("Edad: "))
-    print("Edad registrada:", edad)
-except ValueError:
-    print("Ingresa un valor numérico.")
+from colorama import Fore, Style
+while True:
+    try:
+        edad = int(input("Edad: "))
+        print("Edad Registrada: ", edad)
+        break
+    except ValueError:
+        print(Fore.RED + "Error: Debe ingresar un número entero." + Style.RESET_ALL)
+        print(Style.RESET_ALL)
+print(Fore.GREEN + "Edad ingresada correctamente." + Style.RESET_ALL)
